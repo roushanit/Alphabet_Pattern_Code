5995d12c788cc41be475e73f85f96a8a759ba61b
roushanit <roushansingh1992@gmail.com>
2025-06-24 08:27:55 +0530

    alphabet M code

---
+++ F.py
@@ -0,0 +1,7 @@
+for r in range(7):
+    for c in range(5):
+        if (c==0) or ((r==0 or r==3) and (c > 0)): 
+            print("*",end='')
+        else:
+            print(" ",end='')
+    print()
